63f9d041206d92d9619c360194c1c893155fe055
webdeveloper1213 <sameera_sulaiman@yahoo.com>
2022-06-01 16:55:06 -0700

    changes madde to App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
 import React  from "react";
 import './App.css';
 
-const App = () => {
-    let name: string;
-    name = "Sameera";
+const App: React.FC = () => {
+//     let name: string;
+//     name = "Sameera";
 
-    let hobbies: string[];
-    hobbies = ['coding ','cooking ','swimming '];
+//     let hobbies: string[];
+//     hobbies = ['coding ','cooking ','swimming '];
 
-    let role: [number , string];
-    role = [1,'1'];
+//     let role: [number , string];
+//     role = [1,'1'];
 
-    type Person = {
-        name: string;
-        age?: number;
-    }
-const person: Person = {
-name: 'Sameera',
-}
-console.log(person)
+//     type Person = {
+//         name: string;
+//         age?: number;
+//     }
+// const person: Person = {
+// name: 'Sameera',
+// }
+// console.log(person)
     
 return (
     <>
-        <p>Hello World</p>
- <p>{name} , {hobbies}</p>
+    <div className="App">
+<span className="heading">Taskify</span>
+</div>
     </>
 
 )
